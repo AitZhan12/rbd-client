8bca144a9eadb67ca814253c1a9b7fbfd048d95a
ayen <ye.akhmetzhanov@ecc.kz>
2026-07-10 11:11:09 +0500

    DOC: - Add time to logging.

--- chane_memo_public.py
+++ chane_memo_public.py
@@ -69,5 +69,7 @@ async def main():
             logging.info(f"all saved")
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s [%(levelname)s] %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
     asyncio.run(main())
